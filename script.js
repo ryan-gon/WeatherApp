fdea50ffe851c73f6ee106100522ad9d68092030
Ryan Matthew Gonzalez <ryanpiunj@gmail.com>
2021-02-19 20:06:04 -0500

    update script.js (2)

--- script.js
+++ script.js
@@ -13,3 +13,29 @@ if (tempStoredSearches != null)
 var today = new Date();
 var currentDate = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
 
+function populateCurrentWeather() {
+
+    $.ajax({
+        url: currentWeatherUrl,
+        method: "GET"
+    }).then(function (response) {
+
+        //Object to store current weather data
+        var currentWeatherObj = {
+            location: response.name,
+            date: currentDate,
+            weatherIcon: response.weather[0].icon,
+            temperature: Math.round(response.main.temp),
+            humidity: response.main.humidity,
+            wind: response.wind.speed,
+            uvIndex: 0,
+            uvIntensity: ""
+        };
+
+        
+        currentWeatherObj.date = formatDates(currentWeatherObj.date);
+
+         
+        var latitude = response.coord.lat;
+        var longitude = response.coord.lon;
+        var currentUvUrl = "https://api.openweathermap.org/data/2.5/uvi?lat=" + latitude + "&lon=" + longitude + "&appid=" + apiKey;
