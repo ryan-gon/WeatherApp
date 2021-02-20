276b7e4f30593ec1d7fdd1de6183a5546db08678
Ryan Matthew Gonzalez <ryanpiunj@gmail.com>
2021-02-19 19:43:46 -0500

    intial commit , index, starter script, style.css

---
+++ script.js
@@ -0,0 +1,8 @@
+var searchBar = $("#search-bar");
+var searchButton = $("#search-btn");
+var searchHistory = $("#search-history");
+var weatherCol = $("#weather-col");
+
+var apiKey = "913db2d2e0e028d3282906a34005c24d";
+
+
