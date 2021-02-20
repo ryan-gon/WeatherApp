6c228238407502acf509da6bee663baa4df75a96
Ryan Matthew Gonzalez <ryanpiunj@gmail.com>
2021-02-19 19:53:09 -0500

    update script.js

--- script.js
+++ script.js
@@ -5,4 +5,11 @@ var weatherCol = $("#weather-col");
 
 var apiKey = "913db2d2e0e028d3282906a34005c24d";
 
+var tempStoredSearches = localStorage.getItem("storedSearches");
+if (tempStoredSearches != null)
+    storedSearches = tempStoredSearches.split(",");
+
+//Creates current date variable
+var today = new Date();
+var currentDate = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
 
